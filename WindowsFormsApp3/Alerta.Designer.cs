7e5865a0a6acbe398f4de6cdb0e220b9b0629c4b
Luiz1n <64563249+luiz1n@users.noreply.github.com>
2021-02-19 18:11:13 -0300

    Add files via upload

---
+++ WindowsFormsApp3/Alerta.Designer.cs
@@ -0,0 +1,112 @@
+﻿using System.ComponentModel;
+
+namespace WindowsFormsApp3
+{
+    partial class Alerta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(120, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "UPDATER";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(55, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 26);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LimeGreen;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(213, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 26);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(143, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 27);
+            this.label1.TabIndex = 5;
+            // 
+            // Alerta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(371, 126);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Location = new System.Drawing.Point(15, 15);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Alerta";
+            this.Text = "uupdater";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.Label label2;
+
+        #endregion
+    }
+}
---
+++ WindowsFormsApp3/Checker.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using WindowsFormsApp3.Properties;
+
+namespace WindowsFormsApp3
+{
+    public class Checker
+    {
+        public static string CheckerUpdater()
+        {
+            WebClient wc = new WebClient();
+            string version_Program = "1.0";
+            string Body = wc.DownloadString("https://pastebin.com/raw/fYNM8s4Y");
+
+            if (Body.Contains(version_Program))
+            {
+                return "true";
+            }
+            else
+            {
+                return Body;
+            }
+
+        }
+        
+        public static void UpdaterV()
+        {
+            if (CheckerUpdater() != "true")
+            {
+                Alerta alertaNitro = new Alerta();
+                alertaNitro.ShowWindowNitroAlert();
+            }
+        }
+    }
+}
